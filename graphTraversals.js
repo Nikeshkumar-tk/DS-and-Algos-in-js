a28ba930cbcf1bfa35f561000f2f4ebccf75acd7
Nikeshkumar TK <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-04-02 15:26:02 +0000

    Initial commit

---
+++ graphTraversals.js
@@ -0,0 +1,20 @@
+//Depth first using stack
+
+const depthFirst = (graph, source) => {
+  const stack = [source]
+  while(stack.length > 0){
+  const current = stack.pop()
+  console.log(current)
+  graph[current]?.forEach((node) => {
+    stack.push(node)
+  })
+  }
+}
+
+const graph = {
+  a:["b","c"],
+  b:["d"],
+  c:["e","f"]
+}
+
+depthFirst(graph, "a")
