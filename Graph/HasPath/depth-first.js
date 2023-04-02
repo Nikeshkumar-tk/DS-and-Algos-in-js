142590ea31b1e9b0d3036eae9c017062e79dc0be
Nikeshkumar TK <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-04-02 16:13:13 +0000

    hasPath added

---
+++ Graph/HasPath/depth-first.js
@@ -0,0 +1,15 @@
+const hasPath = (graph, root, dest) => {
+  if(root === dest) return true
+  graph[root]?.forEach((node) => {
+    if(hasPath(graph, node, dest) === true) return true
+  })
+  return false
+}
+
+const graph = {
+  a:["b","c"],
+  b:["d"],
+  c:["e","f"]
+}
+
+console.log(hasPath(graph, "a", "c"))
