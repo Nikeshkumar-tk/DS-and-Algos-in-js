efd4eb32481ebc5acfd6d5d61e4b71934f03274e
Nikeshkumar TK <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-04-10 15:59:58 +0000

    Variable basics added

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+// **************************************************** Variables in javascript ****************************************************************
+
+//In programming variables are used for identifying memory locations
+//The way variables are declared and used depends up on the programming language that we are dealing with.
+// In javascript variables are declared using the keywords var, let and const
+
+//Variable declaration using let
+//Variables declared using let have block scope
+//we can reassign values to that variables within its scope
+//The below example will print "Typescript" in the console, since we are reassigning the string "Typescript to the variable name"
+
+let name = "Javascript"
+name = "Typescript"
+console.log(name)
+
