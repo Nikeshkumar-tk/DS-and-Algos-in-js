966806cb4a074d7b73dbb35757ff02c0d6edeeac
Nikeshkumar TK <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-04-15 12:23:01 +0000

    Decisin makig added

---
+++ Basics/Decison making/if-else.js
@@ -0,0 +1,25 @@
+/*                                           if else statement in javascript
+                                             --------------------------------
+Syntax
+                   if(condition){
+                   
+                      code block 1;
+                   }else{
+                   
+                      code block 2;
+                      
+                   }
+It is similar to if statement.The only difference is that if the codition eveluates to false, the else block will be executed instead of passing the control to next line
+
+*/
+
+//example:
+
+const age = 17
+if(age >= 18){
+  console.log("Eligible for votting")
+}else{
+  console.log("Not eligible for votting")
+}
+
+//The above example will print "Not elogible for votting" since the value of variable age is 17 which is less than 18
---
+++ Basics/Decison making/if.js
@@ -0,0 +1,22 @@
+/*                                                   if statements in javascript
+                                                     ----------------------------
+  Syntax
+          if(condition){
+          
+              block of code;
+          
+            }
+            
+If the condition given in the parentheses followed by if keyword evaluates to true, then the block of code is executed else, it is skipped and control is transffered to the next line.
+
+ */
+
+//example : 
+
+ const age = 18
+ if(age >= 18){
+   console.log("Eligible for votting")
+ }
+
+//Running the above example will print "Eligible for votting" since tthe value of variable "age" is 18
+
