8740af082fe88a8dbe7272a6e7b185c1f37afdf3
Vinayak S <vinayaksukhalal@gmail.com>
2023-04-20 12:37:42 +0530

    Modified

---
+++ Basics/Operators/AugmentedOperations.js
@@ -0,0 +1,86 @@
+/*                                               
+Syntax: 
+
+
+        console.log(data to console)
+
+ - "console" is a build-in object in javscript that allows user to console data in the browser or console environment.
+ - The "log" method of console is used for logging data.Eg:console.log(data)
+ - consoling is very helpul while we debug our code to trace the error
+
+                                                Augmented Addition
+                                       ----------------------------------------
+
+*/
+
+//Example : 
+var a=5;
+var b=7;
+var c=2;
+
+a +=12        //  a = a + 12
+b +=9           //  b= b + 9
+c +=7             //   c = c + 7 
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+/*                                               
+
+                                                Augmented Substration
+                                       ----------------------------------------
+
+*/
+
+//Example : 
+var a=25;
+var b=27;
+var c=22;
+
+a -=12        //  a = a - 12
+b -=9           //  b= b - 9
+c -=7             //   c = c - 7 
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+
+/*                                               
+
+                                                Augmented Multiplication
+                                       ----------------------------------------
+
+*/
+
+//Example : 
+var a=25;
+var b=27;
+var c=22;
+
+a *=12        //  a = a * 12
+b *=9           //  b = b * 9
+c *=7             //   c = c * 7 
+console.log(a)
+console.log(b)
+console.log(c)
+
+/*                                               
+
+                                                Augmented Division
+                                       ----------------------------------------
+
+*/
+
+//Example : 
+var a=25;
+var b=27;
+var c=22;
+
+a /=5        //  a = a / 12
+b /=3           //  b = b / 9
+c /=2             //   c = c / 7 
+console.log(a)
+console.log(b)
+console.log(c)
---
+++ Basics/Operators/arithematicOperators.js
@@ -0,0 +1,59 @@
+/*                                               
+Syntax: 
+
+
+        console.log(data to console)
+
+ - "console" is a build-in object in javscript that allows user to console data in the browser or console environment.
+ - The "log" method of console is used for logging data.Eg:console.log(data)
+ - consoling is very helpul while we debug our code to trace the error
+
+                                                  Addition
+                                       ----------------------------------------
+
+*/
+
+//Example : 
+var sum=10+5;
+console.log(sum)
+/*
+                                                 Substraction
+                                     ----------------------------------------
+*/
+//Example : 
+var difference=45-34;
+console.log(difference)
+
+/*
+                                                 Multiplication
+                                     ----------------------------------------
+*/
+//Example : 
+var product=5*6;
+console.log(product)
+
+
+/*
+                                                   Division
+                                     ----------------------------------------
+*/
+//Example : 
+var quotient=50/5;
+console.log(quotient)
+
+/*
+                                                 Multiplication
+                                     ----------------------------------------
+*/
+//Example : 
+var product=5*6;
+console.log(product)
+
+
+/*
+                                                   Modulus Operator
+                                     ----------------------------------------
+*/
+//Example : 
+var remainder=50%5;
+console.log(remainder)
